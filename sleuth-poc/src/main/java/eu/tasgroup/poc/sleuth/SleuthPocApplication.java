1d3092b17f787818abf8cf39405517d806128bdf
tassoc <claudio.tasso@tasgroup.eu>
2022-03-10 09:10:18 +0100

    initial commit

---
+++ sleuth-poc/src/main/java/eu/tasgroup/poc/sleuth/SleuthPocApplication.java
@@ -0,0 +1,13 @@
+package eu.tasgroup.poc.sleuth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SleuthPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SleuthPocApplication.class, args);
+	}
+
+}
---
+++ sleuth-poc/src/test/java/eu/tasgroup/poc/sleuth/SleuthPocApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.tasgroup.poc.sleuth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SleuthPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
